ab6f261c5e45b2620bf6aec47afabdbdc2124d54
Ilya <simonewarlet@gmail.com>
2024-11-20 15:10:30 +0200

    Fix endless while loop with no exit point

--- recursive_finder.rb
+++ recursive_finder.rb
@@ -2,8 +2,8 @@
 class RecursiveFinder
     def last_nested_hash_value(hash)
         last_value = hash.flatten[-1]
-        while last_value.class != Array {}
-            
+        while last_value.class != Array
+            last_value = last_value.flatten[-1]
         end
         last_value
     end
