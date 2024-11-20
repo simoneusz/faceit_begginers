d11ca2196e82066608f0b3e49f44f32a04875168
Ilya <simonewarlet@gmail.com>
2024-11-20 14:45:12 +0200

    Add method that counts types of itemes classes in nested array

---
+++ classes_counter.rb
@@ -0,0 +1,13 @@
+def get_all(arrays, object_name)
+    result = []
+    arrays.each do | line |
+        line.each { |i| result << i if i.class == object_name }
+    end
+    result
+end
+
+array = [[1, 2, 3, 4, '1'], ['2', '5', '10'], [111, 222, 333, 444], ['i', 'love', 'ruby'], { key: 'value' }, [[['text', 100_000]]]]
+
+p get_all(array, String)
+
+p get_all(array, Integer)
