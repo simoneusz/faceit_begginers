2abc391bebf86ada0c6a6eed8ce055d6700c740c
Ilya <simonewarlet@gmail.com>
2024-11-20 13:51:37 +0200

    Add condition to print circle

--- print_circle.rb
+++ print_circle.rb
@@ -4,6 +4,11 @@ diameter = radius*2
   (diameter+1).times do |j|
     dist = Math.sqrt((i-radius)**2 + (j-radius)**2)
     
+    if dist > radius - 0.5
+      print '*'
+    else
+      print ' '
+    end
   end
   p ""
 end
