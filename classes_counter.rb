11d6d96606988d515d502cded53253851435cc65
Ilya <simonewarlet@gmail.com>
2024-11-21 12:26:00 +0200

    Rework get_all method to handle Array class type

--- classes_counter.rb
+++ classes_counter.rb
@@ -1,7 +1,11 @@
 class ClassesCounter
-  def get_all(arrays, object_name)
-    arrays.flatten.filter do |i|
-      i.instance_of?(object_name)
+  def get_all(array, type)
+    array.each_with_object([]) do |element, result|
+      if element.is_a?(type)
+        result << element
+      elsif element.is_a?(Array)
+        result.concat(get_all(element, type))
+      end
     end
   end
 end
